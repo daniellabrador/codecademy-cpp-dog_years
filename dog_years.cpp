dee7a73120c82b24994a3c8c55deca51770b0fb6
Daniel Labrador <thonydaniellelabrador@gmail.com>
2021-08-30 19:11:25 +0800

    add code, compile cpp file

--- dog_years.cpp
+++ dog_years.cpp
@@ -1,15 +1,24 @@
-// Fun Fact: This would be my third programming language learned in Codecademy.
-
 #include <iostream>
 
-int main(){
-  std::cout<<"DDDD   L     \n";
-  std::cout<<"D   D  L     \n";
-  std::cout<<"D   D  L     \n";
-  std::cout<<"D   D  L     \n";
-  std::cout<<"D   D  L     \n";
-  std::cout<<"D   D  L     \n";
-  std::cout<<"DDDD   LLLLL\n";
+int main() {
+  // Declare and initialize variables.
+  int dog_age, early_years, later_years, human_years;
+
+  // Ask information from user.
+  std::cout<<"Enter your dog's age: ";
+  std::cin>>dog_age;
+
+  // The first two years of a dog’s life count as 21 human years.
+  early_years=21;
+
+  // Each following year counts as 4 human years.
+  later_years = (dog_age-2)*4;
+
+  // Compute dog's human years.
+  human_years=early_years+later_years;
+
+  // Output conversion.
+  std::cout<<"My name is Russell! Ruff ruff, I am "<<human_years<<" years old in human years.\n";
 
   return 0;
 }
